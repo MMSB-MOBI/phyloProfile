b8f80f18bbb1f15c3b15d57350089de0cb012131
Mathis-Etu <104562262+Mathis-Etu@users.noreply.github.com>
2022-06-30 17:12:57 +0200

    take np array instead of pd data frame

--- svd.py
+++ svd.py
@@ -1,18 +1,14 @@
-def svd(df,threshold):
+def svd(array,threshold):
     '''
     Apply the svd method to a score profile matrix to reduce it noise according to a
     certain threshold
     '''
-    df = df.fillna(0.0)
-    df = df.divide(df.max(axis=1),axis='index')
-    print("first normalisation")
-    u, s, vh = np.linalg.svd(df, full_matrices=False)
+    P = np.nan_to_num(array)
+    P_max = np.amax(P,axis=1,keepdims=True)
+    P = np.divide(P,P_max)
+    u, s, vh = np.linalg.svd(P, full_matrices=False)
     s[threshold:]=0.0
     P = np.dot(u * s, vh)
-    print("svd")
     P_norm = np.linalg.norm(P,keepdims=True,axis=0)
     P_u = np.divide(P,P_norm)
-    print("second normalisation")
-    df = pd.DataFrame(P_u,index=df.index,columns=df.columns)
-    print("done")
-    return df
+    return P_u
